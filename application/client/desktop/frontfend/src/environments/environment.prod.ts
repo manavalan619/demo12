9963f4ed7923c2de5c492ff1c76373574b1a1be9
manavalan <manavalanr619@gmail.com>
2021-02-19 15:02:39 +0530

    modify16

--- application/client/desktop/frontfend/src/environments/environment.prod.ts
+++ application/client/desktop/frontfend/src/environments/environment.prod.ts
@@ -1,1 +1,1 @@
-export const environment = {DESKTOP_API: 'http://3.83.8.72:8000/desktop',MOBILE_API: 'http://<Your Domain Name or Live IP address>',production: true};
+export const environment = { DESKTOP_API: `http://3.83.8.72:8000/desktop`, MOBILE_API: `http://<Your Domain Name or Live IP address>`, production: true };
