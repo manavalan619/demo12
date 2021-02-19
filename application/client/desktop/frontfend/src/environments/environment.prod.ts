7a0aa7e359fb3050b6d5f1238edcc3f41721b9ca
manavalan <manavalanr619@gmail.com>
2021-02-19 15:32:36 +0530

    modify20

--- application/client/desktop/frontfend/src/environments/environment.prod.ts
+++ application/client/desktop/frontfend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  DESKTOP_API: 'http://<Your Domain Name or Live IP address>',
+  DESKTOP_API: 'http://54.173.12.94:8000/desktop',
   MOBILE_API: 'http://<Your Domain Name or Live IP address>',
   production: true
 };
--- application/client/desktop/frontfend/src/shared/shared.service.ts
+++ application/client/desktop/frontfend/src/shared/shared.service.ts
@@ -4,6 +4,7 @@ import { environment } from '../environments/environment';
 @Injectable()
 export class SharedService {
 
-     public  = 'http://3.83.8.72:8000/desktop';DESKTOP_API
+     public DESKTOP_API = environment.DESKTOP_API;
     public MOBILE_API = environment.MOBILE_API;
 }
+  
