8d4a62106603d3fa0dc3dbc60bff3a69852557e2
manavalan <manavalanr619@gmail.com>
2021-02-23 10:25:33 +0530

    mano1

--- .github/workflows/task-definition.ts
+++ .github/workflows/task-definition.ts
@@ -93,160 +93,160 @@ module.exports = {
                 "credentialsParameter": ""
             }
         },
-        {
-            "name": "apigateway-3755",
-            "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-nightchech-3755-apigateway:1`,
-            "memoryReservation": "300",
-            "resourceRequirements": null,
-            "essential": true,
-            "portMappings": [
-                {
-                    "containerPort": "8000",
-                    "protocol": "tcp",
-                    "hostPort": "8000"
-                }
-            ],
-            "environmentFiles": null,
-            "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@127.0.0.1:27017/nightchech_3755?authSource=admin"},{"name":"CAMUNDAPOD_URL","value":"http://127.0.0.1:8080"},{"name":"SECURITYURL","value":"http://127.0.0.1:8003"},{"name":"AUTHPROXYURL","value":"http://127.0.0.1:8001"},{"name":"ADMINURL","value":"http://127.0.0.1:8004"},{"name":"CAMUNDAURL","value":"http://127.0.0.1:8002"},{"name":"APIGATEWAY","value":"http://127.0.0.1:8000"}
-            ,{"name":"SCHOOLURL","value":"http://127.0.0.1:8005"}
-            ],
-            "secrets": null,
-            "mountPoints": [],
-            "volumesFrom": null,
-            "hostname": null,
-            "user": null,
-            "workingDirectory": null,
-            "extraHosts": null,
-            "logConfiguration": {
-                "logDriver": "awslogs",
-                "options": {
-                    "awslogs-group": "/ecs/nightchech",
-                    "awslogs-region": "us-east-1",
-                    "awslogs-stream-prefix": "ecs",
-                    "awslogs-create-group": "true"
-                }
-            },
-            "ulimits": null,
-            "dockerLabels": null,
-            "dependsOn": null,
-            "repositoryCredentials": {
-                "credentialsParameter": ""
-            }
-        },        
-        {
-            "name": "authproxy-3755",
-            "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-nightchech-3755-authproxy:1`,
-            "memoryReservation": "300",
-            "resourceRequirements": null,
-            "essential": true,
-            "portMappings": [
-                {
-                    "containerPort": "8001",
-                    "protocol": "tcp",
-                    "hostPort": "8001"
-                }
-            ],
-            "environmentFiles": null,
-            "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@127.0.0.1:27017/nightchech_3755?authSource=admin"},{"name":"CAMUNDAURL","value":"http://127.0.0.1:8002"}],
-            "secrets": null,
-            "mountPoints": [],
-            "volumesFrom": null,
-            "hostname": null,
-            "user": null,
-            "workingDirectory": null,
-            "extraHosts": null,
-            "logConfiguration": {
-                "logDriver": "awslogs",
-                "options": {
-                    "awslogs-group": "/ecs/nightchech",
-                    "awslogs-region": "us-east-1",
-                    "awslogs-stream-prefix": "ecs",
-                    "awslogs-create-group": "true"
-                }
-            },
-            "ulimits": null,
-            "dockerLabels": null,
-            "dependsOn": null,
-            "repositoryCredentials": {
-                "credentialsParameter": ""
-            }
-        },
-        {
-            "name": "camundasvc-3755",
-            "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-nightchech-3755-camunda:1`,
-            "memoryReservation": "300",
-            "resourceRequirements": null,
-            "essential": true,
-            "portMappings": [
-                {
-                    "containerPort": "8002",
-                    "protocol": "tcp",
-                    "hostPort": "8002"
-                }
-            ],
-            "environmentFiles": null,
-            "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@127.0.0.1:27017/nightchech_3755?authSource=admin"},{"name":"CAMUNDAPOD_URL","value":"http://127.0.0.1:8080"},{"name":"SECURITYURL","value":"http://127.0.0.1:8003"},{"name":"AUTHPROXYURL","value":"http://127.0.0.1:8001"},{"name":"ADMINURL","value":"http://127.0.0.1:8004"},{"name":"CAMUNDAURL","value":"http://127.0.0.1:8002"},{"name":"APIGATEWAY","value":"http://127.0.0.1:8000"}],
-            "secrets": null,
-            "mountPoints": [],
-            "volumesFrom": null,
-            "hostname": null,
-            "user": null,
-            "workingDirectory": null,
-            "extraHosts": null,
-            "logConfiguration": {
-                "logDriver": "awslogs",
-                "options": {
-                    "awslogs-group": "/ecs/nightchech",
-                    "awslogs-region": "us-east-1",
-                    "awslogs-stream-prefix": "ecs",
-                    "awslogs-create-group": "true"
-                }
-            },
-            "ulimits": null,
-            "dockerLabels": null,
-            "dependsOn": null,
-            "repositoryCredentials": {
-                "credentialsParameter": ""
-            }
-        },
-        {
-            "name": "securitymanager-3755",
-            "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-nightchech-3755-securitymanager:1`,
-            "memoryReservation": "300",
-            "resourceRequirements": null,
-            "essential": true,
-            "portMappings": [
-                {
-                    "containerPort": "8003",
-                    "protocol": "tcp",
-                    "hostPort": "8003"
-                }
-            ],
-            "environmentFiles": null,
-            "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@127.0.0.1:27017/nightchech_3755?authSource=admin"},{"name":"CAMUNDAPOD_URL","value":"http://127.0.0.1:8080"},{"name":"SECURITYURL","value":"http://127.0.0.1:8003"},{"name":"AUTHPROXYURL","value":"http://127.0.0.1:8001"},{"name":"ADMINURL","value":"http://127.0.0.1:8004"},{"name":"CAMUNDAURL","value":"http://127.0.0.1:8002"},{"name":"APIGATEWAY","value":"http://127.0.0.1:8000"}],
-            "secrets": null,
-            "mountPoints": [],
-            "volumesFrom": null,
-            "hostname": null,
-            "user": null,
-            "workingDirectory": null,
-            "extraHosts": null,
-            "logConfiguration": {
-                "logDriver": "awslogs",
-                "options": {
-                    "awslogs-group": "/ecs/nightchech",
-                    "awslogs-region": "us-east-1",
-                    "awslogs-stream-prefix": "ecs",
-                    "awslogs-create-group": "true"
-                }
-            },
-            "ulimits": null,
-            "dockerLabels": null,
-            "dependsOn": null,
-            "repositoryCredentials": {
-                "credentialsParameter": ""
-            }
-        },
+        // {
+        //     "name": "apigateway-3755",
+        //     "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-nightchech-3755-apigateway:1`,
+        //     "memoryReservation": "300",
+        //     "resourceRequirements": null,
+        //     "essential": true,
+        //     "portMappings": [
+        //         {
+        //             "containerPort": "8000",
+        //             "protocol": "tcp",
+        //             "hostPort": "8000"
+        //         }
+        //     ],
+        //     "environmentFiles": null,
+        //     "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@127.0.0.1:27017/nightchech_3755?authSource=admin"},{"name":"CAMUNDAPOD_URL","value":"http://127.0.0.1:8080"},{"name":"SECURITYURL","value":"http://127.0.0.1:8003"},{"name":"AUTHPROXYURL","value":"http://127.0.0.1:8001"},{"name":"ADMINURL","value":"http://127.0.0.1:8004"},{"name":"CAMUNDAURL","value":"http://127.0.0.1:8002"},{"name":"APIGATEWAY","value":"http://127.0.0.1:8000"}
+        //     ,{"name":"SCHOOLURL","value":"http://127.0.0.1:8005"}
+        //     ],
+        //     "secrets": null,
+        //     "mountPoints": [],
+        //     "volumesFrom": null,
+        //     "hostname": null,
+        //     "user": null,
+        //     "workingDirectory": null,
+        //     "extraHosts": null,
+        //     "logConfiguration": {
+        //         "logDriver": "awslogs",
+        //         "options": {
+        //             "awslogs-group": "/ecs/nightchech",
+        //             "awslogs-region": "us-east-1",
+        //             "awslogs-stream-prefix": "ecs",
+        //             "awslogs-create-group": "true"
+        //         }
+        //     },
+        //     "ulimits": null,
+        //     "dockerLabels": null,
+        //     "dependsOn": null,
+        //     "repositoryCredentials": {
+        //         "credentialsParameter": ""
+        //     }
+        // },        
+        // {
+        //     "name": "authproxy-3755",
+        //     "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-nightchech-3755-authproxy:1`,
+        //     "memoryReservation": "300",
+        //     "resourceRequirements": null,
+        //     "essential": true,
+        //     "portMappings": [
+        //         {
+        //             "containerPort": "8001",
+        //             "protocol": "tcp",
+        //             "hostPort": "8001"
+        //         }
+        //     ],
+        //     "environmentFiles": null,
+        //     "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@127.0.0.1:27017/nightchech_3755?authSource=admin"},{"name":"CAMUNDAURL","value":"http://127.0.0.1:8002"}],
+        //     "secrets": null,
+        //     "mountPoints": [],
+        //     "volumesFrom": null,
+        //     "hostname": null,
+        //     "user": null,
+        //     "workingDirectory": null,
+        //     "extraHosts": null,
+        //     "logConfiguration": {
+        //         "logDriver": "awslogs",
+        //         "options": {
+        //             "awslogs-group": "/ecs/nightchech",
+        //             "awslogs-region": "us-east-1",
+        //             "awslogs-stream-prefix": "ecs",
+        //             "awslogs-create-group": "true"
+        //         }
+        //     },
+        //     "ulimits": null,
+        //     "dockerLabels": null,
+        //     "dependsOn": null,
+        //     "repositoryCredentials": {
+        //         "credentialsParameter": ""
+        //     }
+        // },
+        // {
+        //     "name": "camundasvc-3755",
+        //     "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-nightchech-3755-camunda:1`,
+        //     "memoryReservation": "300",
+        //     "resourceRequirements": null,
+        //     "essential": true,
+        //     "portMappings": [
+        //         {
+        //             "containerPort": "8002",
+        //             "protocol": "tcp",
+        //             "hostPort": "8002"
+        //         }
+        //     ],
+        //     "environmentFiles": null,
+        //     "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@127.0.0.1:27017/nightchech_3755?authSource=admin"},{"name":"CAMUNDAPOD_URL","value":"http://127.0.0.1:8080"},{"name":"SECURITYURL","value":"http://127.0.0.1:8003"},{"name":"AUTHPROXYURL","value":"http://127.0.0.1:8001"},{"name":"ADMINURL","value":"http://127.0.0.1:8004"},{"name":"CAMUNDAURL","value":"http://127.0.0.1:8002"},{"name":"APIGATEWAY","value":"http://127.0.0.1:8000"}],
+        //     "secrets": null,
+        //     "mountPoints": [],
+        //     "volumesFrom": null,
+        //     "hostname": null,
+        //     "user": null,
+        //     "workingDirectory": null,
+        //     "extraHosts": null,
+        //     "logConfiguration": {
+        //         "logDriver": "awslogs",
+        //         "options": {
+        //             "awslogs-group": "/ecs/nightchech",
+        //             "awslogs-region": "us-east-1",
+        //             "awslogs-stream-prefix": "ecs",
+        //             "awslogs-create-group": "true"
+        //         }
+        //     },
+        //     "ulimits": null,
+        //     "dockerLabels": null,
+        //     "dependsOn": null,
+        //     "repositoryCredentials": {
+        //         "credentialsParameter": ""
+        //     }
+        // },
+        // {
+        //     "name": "securitymanager-3755",
+        //     "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-nightchech-3755-securitymanager:1`,
+        //     "memoryReservation": "300",
+        //     "resourceRequirements": null,
+        //     "essential": true,
+        //     "portMappings": [
+        //         {
+        //             "containerPort": "8003",
+        //             "protocol": "tcp",
+        //             "hostPort": "8003"
+        //         }
+        //     ],
+        //     "environmentFiles": null,
+        //     "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@127.0.0.1:27017/nightchech_3755?authSource=admin"},{"name":"CAMUNDAPOD_URL","value":"http://127.0.0.1:8080"},{"name":"SECURITYURL","value":"http://127.0.0.1:8003"},{"name":"AUTHPROXYURL","value":"http://127.0.0.1:8001"},{"name":"ADMINURL","value":"http://127.0.0.1:8004"},{"name":"CAMUNDAURL","value":"http://127.0.0.1:8002"},{"name":"APIGATEWAY","value":"http://127.0.0.1:8000"}],
+        //     "secrets": null,
+        //     "mountPoints": [],
+        //     "volumesFrom": null,
+        //     "hostname": null,
+        //     "user": null,
+        //     "workingDirectory": null,
+        //     "extraHosts": null,
+        //     "logConfiguration": {
+        //         "logDriver": "awslogs",
+        //         "options": {
+        //             "awslogs-group": "/ecs/nightchech",
+        //             "awslogs-region": "us-east-1",
+        //             "awslogs-stream-prefix": "ecs",
+        //             "awslogs-create-group": "true"
+        //         }
+        //     },
+        //     "ulimits": null,
+        //     "dockerLabels": null,
+        //     "dependsOn": null,
+        //     "repositoryCredentials": {
+        //         "credentialsParameter": ""
+        //     }
+        // },
         {
             "name": "school-3755",
             "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-nightchech-3755-school:1`,
